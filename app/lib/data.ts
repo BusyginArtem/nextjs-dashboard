ff7bde3a6a106eb8ccd09424da6799022d4d3574
BusyginArtem <artembusygin87@gmail.com>
2023-11-03 16:10:51 +0100

    chapter 12 - done

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -22,7 +22,7 @@ export async function fetchRevenue() {
     // Don't do this in real life :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 13000));
+    await new Promise((resolve) => setTimeout(resolve, 2500));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -39,7 +39,7 @@ export async function fetchLatestInvoices() {
   try {
     noStore();
 
-    await new Promise((resolve) => setTimeout(resolve, 8500));
+    await new Promise((resolve) => setTimeout(resolve, 1500));
 
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
